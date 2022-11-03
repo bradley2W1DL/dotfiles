8ef9d40c9c65da96827fcb0fe2003075d9e423d9
bradley2w1dl <bradley@himaxwell.com>
2022-11-03 10:09:41 -0600

    Add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+## DEFAULT FISH COLOR OVERRIDES ##
+set fish_color_valid_path cyan # no underline
+set fish_color_command normal
+
+## THEME COLOR OVERRIDES ##
+set pure_color_primary green
+set pure_color_git_branch pure_color_light
+set pure_color_prompt_on_success white 
+
+## asdf version manager (replaces rbenv, nvm, etc.)
+source ~/.asdf/asdf.fish
+
+## ALIASES ##
+alias vim=nvim
+
+function heroku-console
+  heroku run -a $argv[1] -e CONSOLE_USER=$CONSOLE_USER "bundle exec rails console -- --nomultiline"
+end
+
+function set-upstream
+  git branch --set-upstream-to=origin/(git branch --show-current) (git branch --show-current)
+end
+# alias heroku-console='f() { heroku run -a $1 -e CONSOLE_USER=$CONSOLE_USER "bundle exec rails console -- --nomultiline" };f'
+# alias set-upstream="git branch --set-upstream-to=origin/(git branch --show-current) (git branch --show-current)"
+
+set -x CONSOLE_USER bradley@himaxwell.com
+set -x EDITOR nvim
+
