a6d1a61712346c13836b4327a113bedc0cf2d5ef
bradley2w1dl <bradley@himaxwell.com>
2024-07-21 20:24:15 -0600

    fix stow.fish and update stow-ignore

--- laptop/stow.fish
+++ laptop/stow.fish
@@ -6,27 +6,27 @@ if ! type -q stow
   brew install stow
 end
 
-# if .config dir already exists, create backups of any existing dirs that I 
-# want to symlink
+# if .config dir already exists, create backups of any existing dirs that I want to symlink
 if test -d $HOME/.config
   for filepath in (pwd)/.config/*
-  set file (basename $filepath)
-  set CONFIG_DIR $TARGET_DIR/.config
-  set BACKUP_DIR $HOME/.dotfiles_backup
+   set file (basename $filepath)
+   set CONFIG_DIR $HOME/.config
+   set BACKUP_DIR $HOME/.dotfiles_backup
 
-  # file exists in $HOME/.config and is not already a symlink
-  if test -d $CONFIG_DIR/$file && test ! -L $CONFIG_DIR/$file
-    if test ! -d $BACKUP_DIR
-      echo "📁 creating .dotfiles_backup directory"
-      mkdir $BACKUP_DIR
-    end
+   # file exists in $HOME/.config and is not already a symlink
+   if test -d $CONFIG_DIR/$file && test ! -L $CONFIG_DIR/$file
+     if test ! -d $BACKUP_DIR
+       echo "📁 creating .dotfiles_backup directory"
+       mkdir $BACKUP_DIR
+     end
 
-    echo "~/.config/$file exists, backing up now"
-    cp -R $CONFIG_DIR/$file $BACKUP_DIR/
-    if test $status -eq 0
-      echo "...and remove existing"
-      rm -rf $CONFIG_DIR/$file
-    end
+     echo "~/.config/$file exists, backing up now"
+     cp -R $CONFIG_DIR/$file $BACKUP_DIR/
+     if test $status -eq 0
+       echo "...and remove existing"
+       rm -rf $CONFIG_DIR/$file
+     end
+   end
   end
 end
 
