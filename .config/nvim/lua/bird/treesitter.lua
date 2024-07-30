aa7d3ae924d0fe7573e4e44ce0a48fea4070e6a4
bradley2w1dl <bradley@himaxwell.com>
2024-07-30 12:30:45 -0600

    Add custom treesitter parser for "slim" templates

--- .config/nvim/lua/bird/treesitter.lua
+++ .config/nvim/lua/bird/treesitter.lua
@@ -8,7 +8,8 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- force neovim to recognize .astro file extension as an "Astro" file
 vim.filetype.add({
   extension = {
-    astro = "astro"
+    astro = "astro",
+    slim = "slim",
   }
 })
 
@@ -21,7 +22,7 @@ treesitter_config.setup {
   auto_install = true,
 
   -- List of parsers to ignore (add here for broken parsers)
-  ignore_install = { "" },
+  ignore_install = {},
   autopairs = {
     enabled = true,
   },
@@ -38,3 +39,19 @@ treesitter_config.setup {
 }
 
 -- Hint: if syntax highlighting is broken try renaming the default neovim parser directory
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- Custom parser cloned from git repo
+-- for this to work you must also manually copy the queries from the local repo into
+-- <runtimepath>/nvim-treesitter/queries/slim/
+parser_config.slim = {
+  install_info = {
+    url = "~/workspace/tree-sitter-slim", -- local path or git repo
+    files = {"src/parser.c", "src/scanner.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "master", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  -- filetype = "slim", -- if filetype does not match the parser name
+}
