06ddac01bd33121550aa1f4f64e6aba636b257c4
bradley2w1dl <bberger@joinhomebase.com>
2025-12-08 23:55:58 -0700

    WIP rewrite laptop/setup.sh to be used as bootstrap file

---
+++ .config/yadm/mac_install.fish
@@ -0,0 +1,48 @@
+#!/opt/homebrew/bin/fish
+
+## Scripts to ensure the following programs are installed:
+# nvim
+# rg
+# jq
+# 1Password cli (op)
+# fish (already be installed at this point)
+#  -> oh-my-fish (themes)
+#  -> fisher (plugin management)
+#  -> breeze (scm-breeze-ish)
+# xcode-select
+
+# run brew with local Brewfile
+brew bundle -v
+
+echo "✅ make sure to install 1Password desktop app and sign in."
+
+if ! type -q nvim
+    brew install neovim
+
+    # intall all configured nvim packages (./config/nvim/usr)
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c PackerSync
+end
+
+# Fish plugins
+if ! type -q omf
+    echo 'installing oh-my-fish...'
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+    # install "pure" theme and ensure it's set
+    omf install
+end
+
+if ! test -e (pwd)/.config/fish/functions/fisher.fish
+    echo 'installing Fisher plugin manager...'
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+end
+
+if ! test -e (pwd)/.config/fish/functions/breeze.fish
+    echo 'installing breeze for git shortcuts...'
+    fisher install shinriyo/breeze
+end
+
+if ! type -q xcode-select
+    echo "installing xcode-select dev tools (this is very slow 🐢)"
+    xcode-select -install
+end
